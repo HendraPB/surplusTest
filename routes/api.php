ffd94730ac563bf21c1e19bf7331fb969d580aff
HendraPB <hendrapratama1493@gmail.com>
2023-02-12 22:00:23 +0700

    Create route

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::resource('category', 'CategoryController');
+Route::resource('product', 'ProductController');
+Route::resource('image', 'ImageController');
